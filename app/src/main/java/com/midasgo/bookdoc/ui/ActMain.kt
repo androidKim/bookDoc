f44906c7f2e6ff92edbdddf0bd312fbc0dad126b
taejun <taejun@MacBook-Pro.local>
2019-04-04 11:43:39 +0900

    dummy commit

---
+++ app/src/main/java/com/midasgo/bookdoc/ui/ActMain.kt
@@ -0,0 +1,12 @@
+package com.midasgo.bookdoc.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.midasgo.bookdoc.R
+
+class ActMain : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.act_main)
+    }
+}
